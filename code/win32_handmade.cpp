39d6c1598edadbf9f0c3acdf2b4c2385d0db60af
Naityve <longinflorek@gmail.com>
2023-08-25 00:39:44 +0100

    HMH Day 4, created rgb render and made it animate with PeekMessage loop

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -12,54 +12,106 @@
 #include <windows.h>
 #include <wingdi.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
 // Global for now
 global_variable bool Running;
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
 
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+
+global_variable int BytesPerPixel = 4;
+
+internal void RenderWeirdGradient(int XOffSet, int YOffSet)
+{
+    int Width = BitmapWidth;
+    int Height = BitmapHeight;
+    int Pitch = Width*BytesPerPixel;
+    uint8 *Row = (uint8 *)BitmapMemory; 
+    for(int Y = 0;
+        Y < BitmapHeight;
+        Y++)
+    {
+        uint8 *Pixel = (uint8 *)Row;
+        for(int X = 0;
+            X < BitmapWidth;
+            X++)
+        {
+            //*Pixel = 170;//blue
+            *Pixel = (uint8) (X + XOffSet);
+            Pixel++;
+
+            //*Pixel = 50;//green
+            *Pixel = (uint8) (Y + YOffSet);
+            Pixel++;
+
+            *Pixel = 100; //red
+            Pixel++;
+
+            *Pixel = 0; //pad
+            Pixel++;
+        }
+        Row += Pitch;
+    } 
+}
 
 // Define DIB Section Resize
 
 internal void
 Win32ResizeDIBSection(int Width, int Height)
 {
-    if(BitmapHandle)
+
+    if(BitmapMemory)
     {
-        DeleteObject(BitmapHandle);
-    }
-    
-    if(!BitmapDeviceContext)
-    {
-        BitmapDeviceContext = CreateCompatibleDC(0);
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
 
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight; // positive start lower left, negative start upper left
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-    BitmapHandle = CreateDIBSection(
-        BitmapDeviceContext, &BitmapInfo,
-        DIB_RGB_COLORS,
-        &BitmapMemory,
-        0, 0
-    ); 
+    int BitmapMemorySize = (BitmapWidth * BitmapHeight) * BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+    //TODO : probably clear this to black
 }
 
 internal void
-Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+Win32UpdateWindow(HDC DeviceContext, RECT *ClientRect, int X, int Y, int Width, int Height)
 {
-    StretchDIBits(  DeviceContext, X, Y, Width, Height,
-                    X, Y, Width, Height,
+    /*
+
+    */
+
+    int WindowWidth = ClientRect->bottom - ClientRect->left;
+    int WindowHeight = ClientRect->bottom - ClientRect->top;
+    StretchDIBits(  DeviceContext, 
+                    /*X, Y, Width, Height,
+                    X, Y, Width, Height,*/
+                    0, 0, BitmapWidth, BitmapHeight,
+                    0, 0, WindowWidth, WindowHeight,
                     BitmapMemory,
                     &BitmapInfo,
                     DIB_RGB_COLORS, SRCCOPY);
@@ -111,7 +163,11 @@ Win32MainWindowCallback( HWND Window,
             int Y = Paint.rcPaint.top;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break; 
 
@@ -143,7 +199,7 @@ WinMain(HINSTANCE Instance,
 
     if(RegisterClass(&WindowClass))
     {
-        HWND WindowHandle =
+        HWND Window =
             CreateWindowEx(
                 0,
                 WindowClass.lpszClassName,
@@ -158,22 +214,34 @@ WinMain(HINSTANCE Instance,
                 Instance,
                 0
             );
-        if(WindowHandle)
+        if(Window)
         {
             Running = true;
+            int XOffSet = 0;
+            int YOffSet = 0;
             while(Running)
             {
                 MSG Message;
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if(MessageResult > 0)
+                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                 {
+                    if(Message.message == WM_QUIT)
+                    {
+                        Running = false;
+                    }
+
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
                 }
-                else
-                {
-                    break;
-                }
+                RenderWeirdGradient(XOffSet, YOffSet);
+
+                HDC DeviceContext = GetDC(Window);
+                RECT ClientRect;
+                GetClientRect(Window, &ClientRect);
+                int WindowWidth = ClientRect.right - ClientRect.left;
+                int WindowHeight = ClientRect.bottom - ClientRect.top;
+                Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+                ReleaseDC(Window, DeviceContext);
+                XOffSet++;
             }
         }
         else
---
+++ notes/NOTES.md
@@ -0,0 +1,9 @@
+Bitmap = Bitmap (BMP) is an image file format that can be used to create and store computer graphics. A bitmap file displays a small dots in a pattern that, when viewed from afar, creates an overall image.
+
+DIB = Device Independent Bitmap
+
+Unaligned Accessing x86 Architecture = 
+
+windows little endian memory loading
+
+truncation - TODO
